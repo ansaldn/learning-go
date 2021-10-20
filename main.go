c9f4674335657310ea163d843c4aa4446892087d
David Ansa <david.ansa@paddle.com>
2021-10-20 17:17:48 +0100

    Go... and be basic 101

--- main.go
+++ main.go
@@ -1,6 +1,15 @@
 package main
 
 func main() {
-	println("Hello World")
+	welcome := "Hello World again"
+	var welcome1 string = "hellow world once more"
+	println(welcome)
+	println(welcome1)
+
+	// myAge := 21
+
+	var myAge int = 27
+
+	println("I am " + myAge + " years old")
 
 }
