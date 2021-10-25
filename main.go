3446dc098a7f2a24937114629c840908eefa9ade
David Ansa <david.ansa@paddle.com>
2021-10-25 09:08:22 +0100

    Switch statments for go 101

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 func main() {
-	welcome := "Hello World again"
-	var welcome1 string = "hellow world once more"
-	println(welcome)
-	println(welcome1)
+	// welcome := "Hello World again"
+	// var welcome1 string = "hellow world once more"
+	// println(welcome)
+	// println(welcome1)
 
-	// myAge := 21
+	myAge := 21
 
-	var myAge int = 27
+	// var myAge int = 27
 
-	println("I am " + myAge + " years old")
+	// println("I am " + myAge + " years old")
+
+	switch {
+	case myAge >= 21:
+		println("You are allowed to drink")
+	default:
+		println("You are too young to drink")
+
+	}
 
 }
